1412d069987beb2f78a777cb73e840128836d01a
Leonard Volodarchuk <kenzorleo@gmail.com>
2023-03-15 13:42:50 +0000

    second commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello GOIT world");
+        System.out.println("Hello GOIT world");
+        System.out.println("Hello GOIT world");
+        System.out.println("Hello GOIT world");
+        System.out.println("Hello GOIT world");
+    }
+
+
+}
